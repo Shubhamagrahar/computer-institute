9237b429d1aee8bc3e83ec3f719aa0621cfb4716
Shubham Agrahari <shubhamagrahari311@gmail.com>
2026-03-23 22:18:48 +0530

    chore: removed unused folders (area_s1, area_user, VERIFY) and CDN cache files

--- area_s1/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-//header("location:../login_register/index");
-
-echo '<script>alert("Logout Successfully Done.");window.location.assign("../../");</script>';
-?>
